3f7a78857d3d4a3338a1b3704bd4bf3379a24c3c
ZKJ <songzh@alalei.local>
2017-02-16 15:43:06 +0800

    GCD定时器相关代码提交

---
+++ GCD定时器/GCD定时器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD定时器
+//
+//  Created by ZKJ on 2016/11/24.
+//  Copyright © 2016年 ZKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD定时器/GCD定时器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD定时器
+//
+//  Created by ZKJ on 2016/11/24.
+//  Copyright © 2016年 ZKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
